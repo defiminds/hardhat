2c9612596414f179b7778b79e426ea78bc3a4919
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-04-03 23:26:00 +0100

    Delete mnemonic_gen.ts

--- contracts/mnemonic_gen.ts
+++
@@ -1,4 +0,0 @@
-const { ethers } = require("ethers");
-const wallet = ethers.Wallet.createRandom();
-console.log("Nova conta criada. Endereço da conta:", wallet.address);
-console.log("Mnemonic:", wallet.mnemonic.phrase);
