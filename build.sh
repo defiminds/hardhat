355fb74a9a5605f8bb097303ecb598393a177395
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-04-03 18:07:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,37 +1,19 @@
 #!bin/bash
 build() {
-# Docker Container with HARDHAT
-# Instalação do Docker e criação de um container
-sudo apt-get update
-sudo apt-get install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo docker run -it --name hardhat-container ubuntu:latest
-
-# Instalação do Node.js e do npm
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
-
-# Instalação do Hardhat
-npm install --save-dev hardhat
-
-# Criação de um projeto Hardhat
-npx hardhat init my-project
-
-# Configuração do arquivo hardhat.config.js
-echo "module.exports = {
-  solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  networks: {
-    hardhat: {},
-  },
-}" > my-project/hardhat.config.js
+echo -ne -n -e "# Atualiza a lista de pacotes disponíveis
+apt-get update
+# Instala o Node.js
+apt-get install -y nodejs
+# Instala o npm
+apt-get install -y npm
+# Instala o Hardhat globalmente com npm
+npm install -g hardhat
+# Cria um diretório de trabalho para seus projetos
+mkdir /app
+# Entra no diretório de trabalho
+cd /app
+# Inicializa um novo projeto Hardhat
+npx hardhat init" > install-hardhat.sh && sudo chmod +x install-hardhat.sh
+docker run -it --network=host --rm -v $(pwd):/app node:latest /bin/bash -c "cd /app && ./install-hardhat.sh"
 }
 build
